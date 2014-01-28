aecde857da70646f70bbea6947e348f4169cb04e
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2014-01-28 20:12:40 +0100

    add window creation

--- main.c
+++ main.c
@@ -10,7 +10,18 @@ int main()
 		fprintf(stderr, "Error: %s\n", SDL_GetError());
 		return -1;
 	}
+
+	SDL_Window *win = SDL_CreateWindow("OpenGL learning", 0, 0, 640, 480, SDL_WINDOW_SHOWN);
+	if (!win) {
+		fprintf(stderr, "Error: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	SDL_Delay(2000);
+
+	SDL_DestroyWindow(win);
 	SDL_Quit();
+
 	return 0;
 }
 
