07a575ae46ab3e3857e2196366f5af4b193f6d09
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2014-02-09 20:48:48 +0100

    main: add mipmapping

--- main.c
+++ main.c
@@ -382,7 +382,8 @@ void loop(struct context *ctx)
 	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glGenerateMipmap(GL_TEXTURE_2D);
 
 	glUniform1i(texLoc, 0);
 
