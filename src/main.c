0c3a046c875c1bc3280dd9149ba79c8c60f79b6d
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2020-12-28 22:58:22 +0100

    refactor to use context

--- src/main.c
+++ src/main.c
@@ -5,42 +5,48 @@
 
 #define SDL_ERR SDL_GetError()
 
-SDL_Window* init_win(void) {
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		ERR("SDL_Init: %s\n", SDL_ERR);
-		return NULL;
-	}
+struct context {
+	SDL_Window *win;
+	SDL_GLContext gl_ctx;
+};
 
-	SDL_Window *win = SDL_CreateWindow("SDL Tutorial",
-	                                   SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-					   640, 480,
-					   SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
-	if (win == NULL) {
-		ERR("SDL_CreateWindow: %s\n", SDL_ERR);
-		SDL_Quit();
-		return NULL;
-	}
+struct context *context_create(int width, int height) {
+	struct context *ctx = malloc(sizeof *ctx);
+	if (ERR_ON(ctx == NULL, "malloc failed\n"))
+		goto fail;
 
-	return win;
-}
+	ctx->win = SDL_CreateWindow("SDL Tutorial",
+				    SDL_WINDOWPOS_UNDEFINED,
+				    SDL_WINDOWPOS_UNDEFINED,
+				    width, height,
+				    SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+	if (ERR_ON(ctx->win == NULL, "SDL_CreateWindow: %s\n", SDL_ERR))
+		goto fail_ctx;
 
-void deinit_win(SDL_Window* win) {
-	SDL_DestroyWindow(win);
-	SDL_Quit();
-}
-
-SDL_GLContext init_gl(SDL_Window *win) {
-	SDL_GLContext ctx = SDL_GL_CreateContext(win);
-	if (ctx == NULL) {
-		ERR("SDL_GL_CreateContext: %s\n", SDL_ERR);
-		return NULL;
-	}
-
-	int width, height;
-	SDL_GetWindowSize(win, &width, &height);
-	glViewport(0, 0, width, height);
+	ctx->gl_ctx = SDL_GL_CreateContext(ctx->win);
+	if (ERR_ON(ctx->gl_ctx == NULL,  "SDL_GL_CreateContext: %s\n", SDL_ERR))
+		goto fail_win;
 
 	return ctx;
+fail_win:
+	SDL_DestroyWindow(ctx->win);
+fail_ctx:
+	free(ctx);
+fail:
+	return NULL;
+}
+
+void context_destroy(struct context *ctx) {
+	SDL_GL_DeleteContext(ctx->gl_ctx);
+	SDL_DestroyWindow(ctx->win);
+	free(ctx);
+}
+
+int context_resize(struct context *ctx) {
+	int width, height;
+	SDL_GetWindowSize(ctx->win, &width, &height);
+	glViewport(0, 0, width, height);
+	return 0;
 }
 
 void draw(void) {
@@ -49,7 +55,7 @@ void draw(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
 }
 
-void loop(SDL_Window *win) {
+void loop(struct context *ctx) {
 	int done = 0;
 	while (!done) {
 		SDL_Event event;
@@ -58,16 +64,22 @@ void loop(SDL_Window *win) {
 				done = 1;
 		}
 		draw();
-		SDL_GL_SwapWindow(win);
+		SDL_GL_SwapWindow(ctx->win);
 	}
 }
 
 int main() {
-	SDL_Window* win = init_win();
-	if (win == NULL)
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		ERR("SDL_Init: %s\n", SDL_ERR);
 		return -1;
-	init_gl(win);
-	loop(win);
-	deinit_win(win);
+	}
+
+	struct context *ctx = context_create(640, 480);
+	if (ERR_ON(!ctx, "context_create() failed\n"))
+		return -1;
+	loop(ctx);
+
+	context_destroy(ctx);
+	SDL_Quit();
 	return 0;
 }
